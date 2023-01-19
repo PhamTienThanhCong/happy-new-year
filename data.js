823de7a0efa076c5dd59e8d098833ce8a367a108
congphamtienthanh <congphamtienthanh@gmail.com>
2023-01-20 03:19:20 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,14 @@
+const data_name = [
+    {
+        ma_ten: "cong",
+        name: "Công",
+        message: "Hello, I'm Công",
+        img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+    },
+    {
+        ma_ten: "thanh",
+        name: "Thanh",
+        message: "Hello, I'm Thanh",
+        img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+    }
+]
